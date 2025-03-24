d899e996337ebe3b97c0942fbe23919e7071c75b
akkadate <akkadate@gmail.com>
2025-03-24 15:31:01 +0700

    move to public

--- attendance-server.js
+++ attendance-server.js
@@ -1204,7 +1204,7 @@ app.get('/api/reports/student-attendance', requireAuth, async (req, res) => {
 
 // Static routes สำหรับหน้าต่างๆ
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/scan', (req, res) => {
